ab88a6b9cd460b10bca69113c37763565560c68e
jamyun002 <jamyun2@hotmail.com>
2024-09-29 13:07:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ import time
 
 # bot.py와 bot1.py를 독립적으로 실행
 
-subprocess.Popen(['python', '/home/container/bot/bot.py'])
+subprocess.Popen(['python', '/bot/bot.py'])
 
-subprocess.Popen(['python', '/home/container/bot/bot1.py'])
+subprocess.Popen(['python', '/bot/bot1.py'])
 
-subprocess.Popen(['python', '/home/container/bot/bot2.py'])
+subprocess.Popen(['python', '/bot/bot2.py'])
 
-subprocess.Popen(['python', '/home/container/bot/bot3.py'])
+subprocess.Popen(['python', '/bot/bot3.py'])
 
-subprocess.Popen(['python', '/home/container/bot/bot4.py'])
+subprocess.Popen(['python', '/bot/bot4.py'])
 
-subprocess.Popen(['python', '/home/container/bot/bot5.py'])
+subprocess.Popen(['python', '/bot/bot5.py'])
 
 # 메인 스크립트가 종료되지 않도록 대기
 
